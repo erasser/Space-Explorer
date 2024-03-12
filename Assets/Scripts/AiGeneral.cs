a7e78064d935e5785f9b2faa247095906ebe5f33
Michal Grosser <m.grosser@seznam.cz>
2024-03-12 05:39:08 +0100

    spawn sequence WIP

--- Assets/Scripts/AiGeneral.cs
+++ Assets/Scripts/AiGeneral.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 using static Ship;
 using Random = UnityEngine.Random;
@@ -13,7 +12,8 @@ public class AiGeneral : MonoBehaviour
     public List<Ship> shipsToBeSpawned;
     public List<Transform> spawnLocations;
     const float Spacing = 10;   // TODO: Get from ship size
-    // public List<SingleSpawn> SpawnSequence;
+    static List<SpawnData> actualSpawnSequence;
+    static int actualSpawnSequenceIndex;
     // TODO: Ještě celou formaci otočit podle spawnLocation (rotate around point?)
 
     void Start()
@@ -21,20 +21,24 @@ public class AiGeneral : MonoBehaviour
         aiGeneral = this;
     }
 
-    IEnumerator SpawnEnemies(SpawnData spawnData)  // TODO: Random spawn location from list
+    public void SpawnEnemies()  // TODO: Random spawn location from list
     {
-        yield return new WaitForSeconds(spawnData.DelayBeforeSpawn);
-
-        print(spawnData);
-        
         int sign = - 1;
 
-        for (int i = 0; i < spawnData.ShipsCount; ++i)
+        for (int i = 0; i < actualSpawnSequence[actualSpawnSequenceIndex].ShipsCount; ++i)
         {
             Vector3 pos = new(aiGeneral.spawnLocations[0].position.x + GetSign() * i * Spacing, 0, aiGeneral.spawnLocations[0].position.z + Random.Range(-Spacing, Spacing));
-            var newShip = Instantiate(aiGeneral.shipsToBeSpawned[spawnData.ShipType], pos, aiGeneral.spawnLocations[0].rotation);
+            var newShip = Instantiate(aiGeneral.shipsToBeSpawned[actualSpawnSequence[actualSpawnSequenceIndex].ShipType], pos, aiGeneral.spawnLocations[0].rotation);
             newShip.GetComponent<AiPilot>().IWantToAttack(ActiveShip);
         }
+
+        ++actualSpawnSequenceIndex;
+
+        if (actualSpawnSequenceIndex == actualSpawnSequence.Count)
+            return;
+
+        print("invoking index: " + actualSpawnSequenceIndex);
+        aiGeneral.Invoke(nameof(SpawnEnemies), actualSpawnSequence[actualSpawnSequenceIndex].DelayBeforeSpawn);
 
         int GetSign()
         {
@@ -45,25 +49,13 @@ public class AiGeneral : MonoBehaviour
 
     public void SpawnSequence(List<SpawnData> spawnSequence)
     {
-        // foreach (SpawnData spawnData in spawnSequence)
-        // {
-            print("initiating delay");
+            print("initiating spawn list");
             // Task.Delay(500).ContinueWith(task => SpawnEnemies(spawnSequence[0]));
-            // aiGeneral.Invoke(nameof(test), 10);
-            
-            StartCoroutine(SpawnEnemies(spawnSequence[0]));
 
-            // }
+            actualSpawnSequence = spawnSequence;
+            actualSpawnSequenceIndex = 0;
+            // aiGeneral.Invoke(nameof(SpawnEnemies), spawnSequence[0].DelayBeforeSpawn);
+            aiGeneral.Invoke(nameof(SpawnEnemies), 2);
     }
-    
-    // IEnumerator MyFunction(bool status, float delayTime)
-    // {
-    //     yield return new WaitForSeconds(delayTime);
-    //     print("hovno");
-    // }
-    //
-    // public static void test(float a)
-    // {
-    //     print("huh " + a);
-    // }
+
 }
